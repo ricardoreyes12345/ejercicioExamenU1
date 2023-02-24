ff262b5fc723ca67175b8bfc2ceed0fc82d41cf4
ricardo12345 <L22390711@chetumal.tecnm.mx>
2023-02-24 10:52:04 -0500

    subir el examen

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
+import java.util.Scanner;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner lectura = new Scanner(System.in);
+
+        int numeros1;
+        int numeros2;
+
+        System.out.println("introduce el primer numero");
+        int numero1 = lectura.nextInt();
+
+        System.out.println("introduce el segundo numero ");
+        int numero2 = lectura.nextInt();
+
+        double resta=numero1-numero2;
+        double resultado1= numero1/resta;
+        double resultado2=numero2/resta;
+
+       System.out.println("el resultado de la resta de los numeros  es: " +resta);
+       System.out.println("el resultado del prime numero es " +resultado1+ " el resulatdo de segundo valor: "+resultado2);
+
+
+
+
     }
-}
+    }
